bb230036cb2a54656dfd3156a1e99cd6b048c98c
涂 家 浩 <35959817+Jahhow@users.noreply.github.com>
2020-12-22 17:37:03 +0800

    segmentation

--- auto_segmentation.py
+++ auto_segmentation.py
@@ -21,15 +21,44 @@ V1 = ecg.ecg(V1_raw, fields['fs'], show=False)
 # log
 print('\n'+'-'*20 + 'MLII' + '-'*20)
 print('Raw:', MLII_raw)
+print('Num Samples:', len(MLII_raw))
 print('Segmentation score:', ecg.compare_segmentation(annotation.sample, MLII['rpeaks'])['performance'])
+
 print('\n'+'-'*20 + 'V1' + '-'*20)
 print('Raw:', V1_raw)
+print('Num Samples:', len(V1_raw))
 print('Segmentation score:', ecg.compare_segmentation(annotation.sample, V1['rpeaks'])['performance'])
 
 print('\n'+'-'*20 + 'Annotation' + '-'*20)
 print('Number of annotations:', len(annotation.sample))
 print('Locations:', annotation.sample)
-print('Labels:', annotation.symbol[:10])
+print('Labels:', annotation.symbol[:10]) # https://archive.physionet.org/physiobank/database/html/mitdbdir/intro.htm#annotations
+
+#segmentation
+nSamplePerHB = 252
+nSampleBeforePeak = 90
+MLII_Segments=[]
+V1_Segments=[]
+for peakIndice in annotation.sample[:10]:
+    startIndice = peakIndice - nSampleBeforePeak
+    endIndice = startIndice + nSamplePerHB
+    if startIndice < 0:
+        endIndice -= startIndice
+        startIndice = 0
+    elif endIndice > len(MLII_raw):
+        startIndice -= endIndice - len(MLII_raw)
+        endIndice = len(MLII_raw)
+    MLII_Segments.append(MLII_raw[startIndice:endIndice])
+    V1_Segments.append(V1_raw[startIndice:endIndice])
+
+#plot segments
+nSegToPlot=5
+fig, axs = plt.subplots(2, nSegToPlot, figsize=(10, 5), sharex=True, sharey=True)
+for i in range(5):
+    axs[0, i].plot(MLII_Segments[i])
+    axs[1, i].plot(V1_Segments[i])
+    axs[1, i].set_title(annotation.symbol[i])
+plt.show()
 
 # help(wfdb.Annotation)
 # wfdb.plot_wfdb(record=record_preview, annotation=annotation_preview, plot_sym=True,
