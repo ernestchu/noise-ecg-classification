71819a3bc42d39d9330bb72b5cd443fdf7b20819
ernestchu <b073040018@student.nsysu.edu.tw>
2020-12-22 19:16:40 +0800

    get data testing

--- src/get_data.py
+++ src/get_data.py
@@ -1,13 +1,17 @@
 import h5py
 import matplotlib.pyplot as plt
-snr_check = []
-for i in range(2300):
-    with h5py.File('../data/118e24/'+str(i)+'.h5', 'r') as f:
-        # print(len(f['MLII'][:]))
-        # print(len(f['V1'][:]))
-        # print(str(f['label'][()], 'utf-8'))
-        # print(str(f['SNR'][()], 'utf-8'))
-        snr_check.append(0 if str(f['SNR'][()], 'utf-8')=='no' else 1)
+# snr_check = []
+# for i in range(2300):
+#     with h5py.File('../data/118e24/'+str(i)+'.h5', 'r') as f:
+#         snr_check.append(0 if str(f['SNR'][()], 'utf-8')=='no' else 1)
 
-plt.plot(range(len(snr_check)), snr_check)
+ecg = None
+with h5py.File('../data/118e_6/1400.h5', 'r') as f:
+    print(len(f['MLII'][:]))
+    print(len(f['V1'][:]))
+    print(str(f['label'][()], 'utf-8'))
+    print(str(f['SNR'][()], 'utf-8'))
+    ecg = f['MLII'][:]
+
+plt.plot(range(len(ecg)), ecg)
 plt.show()
