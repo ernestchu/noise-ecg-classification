0e3b9312b2075f375de82d9536fefc49718a6b6a
ernestchu <b073040018@student.nsysu.edu.tw>
2020-12-22 22:42:03 +0800

    add id

--- src/gen_data.py
+++ src/gen_data.py
@@ -21,6 +21,7 @@ filename_prefix = '../data/'+filename[-6:]+'/'
 os.mkdir(filename_prefix)
 for i, sample in enumerate(annotation.sample):
     with h5py.File(filename_prefix+str(i)+'.h5', 'w') as f:
+        f.create_dataset('id', data=i)
         f.create_dataset('MLII', data=MLII['filtered'][int(sample-0.25*fs):int(sample+0.45*fs)])
         f.create_dataset('V1', data=V1['filtered'][int(sample-0.25*fs):int(sample+0.45*fs)])
         f.create_dataset('label', data=annotation.symbol[i])
