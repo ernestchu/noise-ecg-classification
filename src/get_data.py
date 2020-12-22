f71cfc6dc738ee69a77d0ff537d02478c475bb82
ernestchu <b073040018@student.nsysu.edu.tw>
2020-12-22 18:51:21 +0800

    gen data

---
+++ src/get_data.py
@@ -0,0 +1,13 @@
+import h5py
+import matplotlib.pyplot as plt
+snr_check = []
+for i in range(2300):
+    with h5py.File('../data/118e24/'+str(i)+'.h5', 'r') as f:
+        # print(len(f['MLII'][:]))
+        # print(len(f['V1'][:]))
+        # print(str(f['label'][()], 'utf-8'))
+        # print(str(f['SNR'][()], 'utf-8'))
+        snr_check.append(0 if str(f['SNR'][()], 'utf-8')=='no' else 1)
+
+plt.plot(range(len(snr_check)), snr_check)
+plt.show()
