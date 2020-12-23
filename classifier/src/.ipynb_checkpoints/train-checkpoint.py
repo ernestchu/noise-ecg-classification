188dbe294a3c34504c5e5256b28f23ac614fe9ae
ernestchu <b073040018@student.nsysu.edu.tw>
2020-12-24 03:49:39 +0800

    trained epoch 50

---
+++ classifier/src/.ipynb_checkpoints/train-checkpoint.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import h5py
+import os
+from ECG_classifier import Model
+
+encoding = '.NLRAaJSVF[!]ejE/fxQ|'
+
+class NSTDB(torch.utils.data.Dataset):
+    def __init__(self):
+        self.h5_list = []
+        for root, dirs, files in os.walk('../segmentation'):
+            for f in files:
+                path = os.path.join(root, f)
+                with h5py.File(path, 'r') as h5:
+                    if h5['MLII'][:].size != 252:
+                        continue
+                self.h5_list.append(os.path.join(root, f))
+        
+    def __getitem__(self, index):
+        with h5py.File(self.h5_list[index], 'r') as f:
+            index = f['index'][()]
+            MLII = torch.tensor(f['MLII'][:])
+            label = torch.tensor(encoding.index(f['label'][()]))
+            return {
+                'index': index,
+                'MLII': MLII.float(),
+                'label': label,
+            }
+    def __len__(self):
+        return len(self.h5_list)
+dataset = NSTDB()
+
+index, MLII, label = dataset[10000].values()
+print('Label:', label)
+print('Length:', len(dataset))
+
+dataset_train, dataset_val = torch.utils.data.random_split(dataset, [int(0.7*len(dataset)), len(dataset)-int(0.7*len(dataset))])
+print(len(dataset_train), len(dataset_val))
+from torch.utils.data import DataLoader
+BATCH_SIZE = 1024
+train_loader = DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(dataset_val, batch_size=BATCH_SIZE, shuffle=True)
+
+model = Model().cuda()
+
+criterion = nn.CrossEntropyLoss().cuda()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3, momentum=0.9)
+
+def train(input_data, model, criterion, optimizer):
+    model.train()
+    loss_list = []
+    total_count = 0
+    acc_count = 0
+    for data in input_data:
+        index, MLII, label = data['index'], data['MLII'].cuda(), data['label'].cuda()
+        
+        optimizer.zero_grad()
+        out = model(MLII)
+        loss = criterion(out, label)
+        loss.backward()
+        optimizer.step()
+        
+        _, pred = torch.max(out, 1)
+        total_count += label.shape[0]
+        acc_count += (pred == label).sum().item()
+        loss_list.append(loss.item())
+        
+    acc = acc_count/total_count
+    loss = sum(loss_list)/len(loss_list)
+    return acc, loss
+
+def val(input_data, model, criterion):
+    model.train()
+    loss_list = []
+    total_count = 0
+    acc_count = 0
+    with torch.no_grad():
+        for data in input_data:
+            index, MLII, label = data['index'], data['MLII'].cuda(), data['label'].cuda()
+        
+            out = model(MLII)
+            loss = criterion(out, label)
+
+            _, pred = torch.max(out, 1)
+            total_count += label.shape[0]
+            acc_count += (pred == label).sum().item()
+            loss_list.append(loss.item())
+        
+    acc = acc_count/total_count
+    loss = sum(loss_list)/len(loss_list)
+    return acc, loss
+
+max_epochs = 50
+log_interval = 1
+
+train_acc_list = []
+train_loss_list = []
+val_acc_list = []
+val_loss_list = []
+
+min_loss = 1000000
+best_model = None
+
+print("Start training...")
+for epoch in range(1, max_epochs+1):
+    train_acc, train_loss = train(train_loader, model, criterion, optimizer)
+    val_acc, val_loss = val(val_loader, model, criterion)
+    
+    train_acc_list.append(train_acc)
+    train_loss_list.append(train_loss)
+    val_acc_list.append(val_acc)
+    val_loss_list.append(val_loss)
+    
+    if val_loss < min_loss:
+        min_loss = val_loss
+        best_model = model.state_dict()
+        
+    if epoch % log_interval == 0:
+        print('')
+        print('=' * 20, 'Epoch', epoch, '=' * 20)
+        print('Train Acc: {:.6f} Train Loss: {:.6f}'.format(train_acc, train_loss))
+        print('  Val Acc: {:.6f}   Val Loss: {:.6f}'.format(val_acc, val_loss))
+        
+torch.save(best_model, 'ECG_classifier_params.pth')
+        
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 4))
+plt.plot(range(len(train_loss_list)), train_loss_list)
+plt.plot(range(len(val_loss_list)), val_loss_list, c='r')
+plt.legend(['train_loss', 'val'])
+plt.title('Loss')
+plt.savefig('Loss.png')
+plt.figure(figsize=(12, 4))
+plt.plot(range(len(train_acc_list)), train_acc_list)
+plt.plot(range(len(val_acc_list)), val_acc_list, c='r')
+plt.legend(['train', 'val'])
+plt.savefig('Acc.png')
---
+++ src/.ipynb_checkpoints/auto_segmentation-checkpoint.py
@@ -0,0 +1,68 @@
+import wfdb
+from biosppy.signals import ecg
+import numpy as np
+import matplotlib.pyplot as plt
+filename = '../mit-bih-noise-stress-test-database-1.0.0/118e_6'
+record_preview = wfdb.rdrecord(filename, sampto=3000)
+annotation_preview = wfdb.rdann(filename, 'atr', sampto=3000)
+signal, fields = wfdb.rdsamp(filename)
+annotation = wfdb.rdann(filename, 'atr')
+
+MLII_raw = signal[:, 0]
+# V1_raw = signal[:, 1]
+
+MLII = ecg.ecg(MLII_raw, fields['fs'], show=False)
+# V1 = ecg.ecg(V1_raw, fields['fs'], show=False)
+
+# plt.plot(MLII['templates_ts'], MLII['templates'].mean(axis=0))
+# plt.title('Heartbeat template')
+# plt.show()
+
+# log
+print('\n'+'-'*20 + 'MLII' + '-'*20)
+print('Raw:', MLII_raw)
+print('Num Samples:', len(MLII_raw))
+print('Num rpeeks:', len(MLII['rpeaks']))
+print('Num Heartbeats:', len(MLII['heart_rate_ts']))
+print('Segmentation score:', ecg.compare_segmentation(annotation.sample, MLII['rpeaks'])['performance'])
+
+# print('\n'+'-'*20 + 'V1' + '-'*20)
+# print('Raw:', V1_raw)
+# print('Num Samples:', len(V1_raw))
+# print('Segmentation score:', ecg.compare_segmentation(annotation.sample, V1['rpeaks'])['performance'])
+
+print('\n'+'-'*20 + 'Annotation' + '-'*20)
+print('Number of annotations:', len(annotation.sample))
+print('Locations:', annotation.sample)
+print('Labels:', annotation.symbol[:10]) # https://archive.physionet.org/physiobank/database/html/mitdbdir/intro.htm#annotations
+
+#segmentation
+nSamplePerHB = 252
+nSampleBeforePeak = 90
+MLII_Segments=[]
+# V1_Segments=[]
+for peakIndice in annotation.sample[:10]:
+    startIndice = peakIndice - nSampleBeforePeak
+    endIndice = startIndice + nSamplePerHB
+    if startIndice < 0:
+        endIndice -= startIndice
+        startIndice = 0
+    elif endIndice > len(MLII_raw):
+        startIndice -= endIndice - len(MLII_raw)
+        endIndice = len(MLII_raw)
+    MLII_Segments.append(MLII_raw[startIndice:endIndice])
+    # V1_Segments.append(V1_raw[startIndice:endIndice])
+
+#plot segments
+nSegToPlot=5
+fig, axs = plt.subplots(1, nSegToPlot, figsize=(10, 3), sharex=True, sharey=True)
+for i in range(5):
+    axs[i].plot(MLII_Segments[i])
+    # axs[, i].plot(V1_Segments[i])
+    axs[i].set_title(annotation.symbol[i])
+# plt.show()
+
+# help(wfdb.Annotation)
+# wfdb.plot_wfdb(record=record_preview, annotation=annotation_preview, plot_sym=True,
+#                time_units='seconds', title='MIT-BIH Record 100',
+#                figsize=(10,4), ecg_grids='all')
