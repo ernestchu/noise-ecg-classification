4e36e34fe7a43da191cceb8cb9b79d808043db01
ernestchu <b073040018@student.nsysu.edu.tw>
2021-01-06 05:22:49 +0800

    classifier

--- classifier/src/.ipynb_checkpoints/ECG_classifier-checkpoint.py
+++ classifier/src/.ipynb_checkpoints/ECG_classifier-checkpoint.py
@@ -4,25 +4,21 @@ encoding = 'NAVR'
 class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
-        self.fc = nn.Sequential(
-            nn.Linear(252, 500),
-            nn.ReLU(),
-            nn.BatchNorm1d(500),
-            nn.Dropout(0.3),
-            nn.Linear(500, 1000),
-            nn.ReLU(),
-            nn.BatchNorm1d(1000),
-            nn.Dropout(0.3),
-            nn.Linear(1000, 1000),
-            nn.ReLU(),
-            nn.BatchNorm1d(1000),
-            nn.Dropout(0.3),
-            nn.Linear(1000, 500),
-            nn.ReLU(),
-            nn.BatchNorm1d(500),
-            nn.Dropout(0.3),
-            nn.Linear(500, len(encoding))
+        def convBlock(in_filters, out_filters, normalization=True):
+            layers = [nn.Conv1d(in_filters, out_filters, 4, stride=2, padding=1)]
+            if normalization:
+                layers.append(nn.BatchNorm1d(out_filters))
+            layers.append(nn.Tanh())
+            return layers
+        self.conv = nn.Sequential(
+            *convBlock(1, 64, normalization=False),
+            *convBlock(64, 128),
+            *convBlock(128, 256),
+            *convBlock(256, 512),
+            nn.ConstantPad1d((0, 1), 0),
+            nn.Conv1d(512, len(encoding), 4, padding=1, bias=False),
+            nn.AdaptiveAvgPool1d(1)
         )
     def forward(self, x):
-        x = self.fc(x)
-        return x
+        x = self.conv(x)
+        return x.reshape(x.shape[0], x.shape[1])
